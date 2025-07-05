72635966c34abd7433766fa53256d3bd581ecb73
Filipe Stefano <filipe.ghisi@gmail.com>
2025-07-05 20:28:54 -0300

    Reviewed, documented fire particles.

--- src/scenes/Fire.ts
+++ src/scenes/Fire.ts
@@ -4,16 +4,19 @@ import { FIRE_EMITTER, Scene } from '../GameConstants';
 
 export default class Fire extends PIXI.Container implements Scene {
   public start(): void {
+    // Creates an Emitter object with the definitions found in config object.
     const emitter = new Emitter(this, upgradeConfig(FIRE_EMITTER, ['particle']));
 
     emitter.updateOwnerPos(window.innerWidth / 2 - 40, window.innerHeight / 2 + 100);
     let elapsed = Date.now();
 
+    // Updates emitter based on time with low rou.
+    const rateOfUpdate = 0.001;
     const update = function up() {
       requestAnimationFrame(update);
       const now = Date.now();
 
-      emitter.update((now - elapsed) * 0.001);
+      emitter.update((now - elapsed) * rateOfUpdate);
       elapsed = now;
     };
 
@@ -22,6 +25,8 @@ export default class Fire extends PIXI.Container implements Scene {
     update();
 
     window.onpointerdown = (event) => {
+      // Creates a PIXI point on screen with pointer coordinates, before
+      // passing it to this PIXI object and figuring out its local ones.
       const clickPosition = new PIXI.Point(event.x, event.y);
       const { x, y } = this.toLocal(clickPosition);
 
